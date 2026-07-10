22c5803b0f5a4b677e6cefce02a192089d6b1664
onc-limb <108280144+onc-limb@users.noreply.github.com>
2026-07-10 11:26:56 +0900

    feat: add server-phoenix-scaffold metric (#11)

---
+++ server/lib/logavo_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule LogavoWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use LogavoWeb, :controller` and
+  `use LogavoWeb, :live_view`.
+  """
+  use LogavoWeb, :html
+
+  embed_templates "layouts/*"
+end
